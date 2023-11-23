efe41dc70be396d602d11d56906ee4f0abed0cc7
abhishek9373 <abhishekgund500@gmail.com>
2023-11-24 03:36:19 +0530

    Added server url's and other constants

---
+++ src/app/config/shared.config.ts
@@ -0,0 +1,10 @@
+const PORT = 3015;
+
+export const SERVER_URL = "http://campus.saplynx.com";
+export const LOCAL_SERVER_URL = `http://localhost:${PORT}`;
+
+export const SOCKET_CONNECTION_URL = "http://campus/saplynx.com";
+export const APP_VERSION = "1";
+
+
+
